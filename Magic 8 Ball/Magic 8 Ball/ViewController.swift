a33e1ebe7ba08a516c554755dddd0072b7c26e68
adithwip <adithwidyapradipta@gmail.com>
2018-05-20 15:00:52 +0700

    Add assets

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by HIJUP on 20/05/18.
+//  Copyright © 2018 Adith Widya Pradipta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
